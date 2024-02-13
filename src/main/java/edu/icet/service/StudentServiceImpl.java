b9f204426012982315bc3df733e0b01929dab65e
dilshanzz <dilshanwijewardhana999@gmail.com>
2024-02-13 23:28:00 +0530

    Modelmapper

--- src/main/java/edu/icet/service/StudentServiceImpl.java
+++ src/main/java/edu/icet/service/StudentServiceImpl.java
@@ -4,22 +4,32 @@ import edu.icet.Repository.StudentRepository;
 import edu.icet.dto.StudentDto;
 import edu.icet.entity.Student;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
 
 public class StudentServiceImpl implements StudentService {
 
-    @Autowired
-    StudentRepository repository;
 
+     StudentRepository repository;
+
+    ModelMapper mapper;
+
+    @Bean
+    public void setup(){
+        this.mapper = new ModelMapper();
+    }
     @Override
     public void addStudent(StudentDto student) {
-        Student student1 = new Student();
-        student1.setFirstName(student.getFirstName());
-        student1.setLastName(student.getLastName());
-        student1.setContact(student.getContact());
+
+        Student student1 = mapper.map(student,Student.class);
+//        Student student1 = new Student();
+//        student1.setFirstName(student.getFirstName());
+//        student1.setLastName(student.getLastName());
+//        student1.setContact(student.getContact());
 
         repository.save(student1);
     }
