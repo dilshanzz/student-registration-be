093b62e6c6011a627e834fbc4a6e5a708c43ab6b
dilshanzz <dilshanwijewardhana999@gmail.com>
2024-03-06 20:00:21 +0530

    delete method added

--- src/main/java/edu/icet/controller/StudentController.java
+++ src/main/java/edu/icet/controller/StudentController.java
@@ -6,11 +6,10 @@ import edu.icet.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 @RestController
-
 @RequestMapping("student")
 @CrossOrigin
-
 public class StudentController {
 
     @Autowired
@@ -18,6 +17,37 @@ public class StudentController {
     @PostMapping("/add")
     StudentEntity addStudent(@RequestBody Student student){
         System.out.println(student);
+//        try {
+//            Thread.sleep(5000);
+//            return studentService.addStudent(student);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
         return studentService.addStudent(student);
     }
+
+    @GetMapping("search/{id}")
+    Student getStudent(@PathVariable Long id){
+         return studentService.getStudent(id);
+    }
+
+    @GetMapping("/get")
+    List<StudentEntity> getAllStudents(){
+        return studentService.getStudents();
+    }
+
+    @DeleteMapping("/{id}")
+    public String removeStudent(@PathVariable Long id){
+
+       return studentService.removeStudent(id) ? "student removed"
+               : "operation failed" ;
+
+    }
+
+//        boolean isRemoved =  studentService.removeStudent(id);
+//        if( isRemoved){
+//            return ("Student Removed");
+//        }
+//        return ("operation failed");
+//    }
 }
--- src/main/java/edu/icet/service/StudentService.java
+++ src/main/java/edu/icet/service/StudentService.java
@@ -3,6 +3,14 @@ package edu.icet.service;
 import edu.icet.dto.Student;
 import edu.icet.entity.StudentEntity;
 
+import java.util.List;
+
 public interface StudentService {
     StudentEntity addStudent(Student student);
+
+    List<StudentEntity> getStudents();
+
+    Student getStudent(Long id);
+
+    boolean removeStudent(Long id);
 }
